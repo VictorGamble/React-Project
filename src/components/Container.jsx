260c4c8fef5de93e8fcaaf1ea6374994c3589ac0
Victor <Victor.Gamble@outlook.com>
2020-04-14 14:47:10 -0400

    added styles for buttons

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`
+
+export default Container;
