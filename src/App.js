043fcc2a8451c78866f502c0e98d6a6fcd017b01
Victor <Victor.Gamble@outlook.com>
2020-04-15 12:18:11 -0400

    added links starting the navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Container from './components/Container';
 import HomePage from './components/HomePage';
 import BookList from './components/BookList';
+import SignUp from './components/SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './book.css'
+
+
+
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Container>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/:BookList">Search Page</Link>
-            </li>
-          </ul>
-        </Container>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/BookList">Search</Link>
+          </li>
+          <li>
+            <Link to="/SignUp">SignUp</Link>
+          </li>
+        </ul>
         <Route path='/' exact component={HomePage} />
-        <Route path="/BookList" component={BookList}/>
+        <Route path="/BookList" component={BookList} />
+        <Route path="/SignUp" component={SignUp}/>
       </Router>
- 
     </div>
   );
 }
