0522d46219c146c5488e8dfd90185971fe528044
Victor <Victor.Gamble@outlook.com>
2020-04-16 18:03:48 -0400

    added bootstrap styling

--- src/App.js
+++ src/App.js
@@ -13,30 +13,12 @@ import './book.css'
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
+      <nav class="nav nav-pills flex-column flex-sm-row navbar-dark bg-dark">
+  <a class="flex-sm-fill text-sm-center nav-link active" href="/">Home</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="/SignUp">SignUp</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="/Login">Login</a>
+  <a class="flex-sm-fill text-sm-center nav-link " href="#">About</a>
 </nav>
-
-
       <Router>
         <Route path='/' exact component={HomePage} />
         <Route path="/BookList" component={BookList} />
--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -7,25 +7,54 @@ class LogIn extends Component{
         password: ''
     }
     
-    submitHandler = () => {
-        
-   }
+   changeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     
     render() {
+        const { userName, password } = this.state;
         return (
             <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
                 <Container>
-                <form>
-                    <label> Email: <input type="text" name="email" /></label>
-                    <br />
-                    <label>Password: <input type="text" name="password" /></label>
-                    <br />
-                    <Container>   
-                        <button>Submit</button>
-                    </Container> 
-                </form>
-              </Container>
-            </div>
+                    <form>
+                    <div className="form-row align-items-center">
+                        <div className="col-sm-3 my-1">
+                        <label className="sr-only" for="inlineFormInputGroupUsername">Username</label>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                            <div className="input-group-text">@</div>
+                            </div>
+                            <input type="text" className="form-control" name="userName" value={userName} onChange={this.changeHandler} id="inlineFormInputGroupUsername" placeholder="Username"/>
+                        </div>
+                        </div>
+                    
+                        <div className="col-sm-3 my-1">
+                        <label className="sr-only" for="inlineFormInputName">Password</label>
+                        <input type="text" className="form-control" mame="password" value={password} onChange={this.changeHandler} id="inlineFormInputName" placeholder="Jane Doe"/>
+                        </div>
+                                <div className="col-auto my-1">
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" id="autoSizingCheck2"/>
+                            <label className="form-check-label" for="autoSizingCheck2">
+                            Remember me
+                            </label>
+                        </div>
+                        </div>            
+                        <div className="col-auto my-1">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                    </form>
+             </Container>
+          </div>
         )
     }
 }
